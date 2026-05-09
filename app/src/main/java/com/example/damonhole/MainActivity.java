9c9a6c1b8f8f3d2374176049f35a3c58d6966ab1
HaeImDuck <yizheng5946@icloud.com>
2026-05-09 14:55:43 +0800

    Fix ClassCastException: update searchLoadingIndicator type to CircularProgressIndicator

--- app/src/main/java/com/example/damonhole/MainActivity.java
+++ app/src/main/java/com/example/damonhole/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     private PlaylistManager playlistManager;
     private LikedSongsManager likedManager;
-    private com.google.android.material.loadingindicator.LoadingIndicator searchLoadingIndicator;
+    private com.google.android.material.progressindicator.CircularProgressIndicator searchLoadingIndicator;
 
     private SearchHistoryAdapter historyAdapter;
     private final List<String> searchHistory = new ArrayList<>();
